d50713b7705e27f27c82f691035805d3f8cca20b
Siegrain4 <reykazuko@gmail.com>
2024-04-19 17:51:53 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,76 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Todo List</h1>
+    <div>
+      <input type="text" v-model="newTodo" placeholder="Tambahkan todo">
+      <button @click="addTodo">Tambah</button>
+    </div>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">{{ todo }}</li>
+    </ul>
+    <p v-if="isHebat">Hebat!</p>
+    <button class="reset-button" @click="resetTodos">Reset</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      newTodo: '',
+      todos: []
+    };
+  },
+  computed: {
+    isHebat() {
+      return this.todos.length >= 4;
+    },
+    isTodoEmpty() {
+      return this.newTodo.trim() === '';
+    }
+  },
+  methods: {
+    addTodo() {
+      if (!this.isTodoEmpty) {
+        this.todos.push(this.newTodo.trim());
+        this.newTodo = '';
+      }else{
+        alert('Tidak Boleh kosong');
+      }
+    },
+    resetTodos() {
+      this.todos = [];
+    }
+  },
+  watch: {
+    todos: {
+      handler(newTodos) {
+        console.log('Todos changed:', newTodos);
+      },
+      deep: true
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+ul {
+  list-style-type: decimal;
+  margin-left: 20px;
+  margin-top: 10px;
+}
+
+li {
+  margin-bottom: 8px;
+}
+
+p {
+  margin-top: 10px;
+  color: green;
+}
+
+.reset-button {
+  background-color: #dc3545;
+  margin-top: 20px;
 }
 </style>
